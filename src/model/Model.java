29193bb8386167a3a58ed94fac06e9d1866b9c43
师传志 <1767604480@qq.com>
2018-05-30 16:33:30 +0800

    提交代码

---
+++ src/model/Model.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Model {
+	public static String name = null;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		Model.name = name;
+	}
+}
---
+++ src/service/Service.java
@@ -0,0 +1,9 @@
+package service;
+
+import dao.Dao;
+
+public class Service {
+	public static void getName() {
+		Dao.getName();
+	}
+}
